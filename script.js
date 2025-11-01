ac06465af5afa026782feab4fecf153e2a78d655
JaganMohan <mohan.kreddy301@gmail.com>
2025-11-01 07:25:55 +0530

    Taking quick notes

---
+++ script.js
@@ -0,0 +1,46 @@
+let createBtn = document.getElementById("create-note-btn");
+let notesContainer = document.getElementById("notes-container");
+
+function createInputBox() {
+  let inputBox = document.createElement("p");
+  inputBox.className = "input-box";
+  inputBox.contentEditable = "true";
+  let deleteIcon = document.createElement("img");
+  deleteIcon.src = "/images/delete.png";
+  deleteIcon.id = "delete-icon";
+  notesContainer.appendChild(inputBox).appendChild(deleteIcon);
+}
+
+notesContainer.addEventListener("click", function (event) {
+  if (event.target.id === "delete-icon") {
+    event.target.parentElement.remove();
+    saveNotesToLocalStorage();
+  } 
+
+  else if(event.target.tagName === 'P') {
+    document.querySelectorAll('.input-box').forEach(box => {
+        box.addEventListener('focus', function() {
+            console.log("Key up event detected");
+        });
+      
+        
+    });
+  }
+
+});
+
+createBtn.addEventListener("click", function () {
+  createInputBox();
+});
+
+function saveNotesToLocalStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+  console.log(notesContainer.innerHTML);
+}
+
+function shwoNotesFromLocalStorage() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+shwoNotesFromLocalStorage();
+
